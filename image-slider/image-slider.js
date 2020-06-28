7f414611f8e2aaa87480758dc82b279c497b6c7e
josephttran <42635012+josephttran@users.noreply.github.com>
2020-06-27 18:52:19 -0700

    Render new image and fill dot when click on back symbol or forward symbol

--- image-slider/image-slider.js
+++ image-slider/image-slider.js
@@ -1,7 +1,3 @@
-window.addEventListener('DOMContentLoaded', function() {
-  displaySliderImage();
-});
-
 const sliderImages = [
   "./images/fitness1.svg",
   "./images/fitness2.svg",
@@ -10,8 +6,50 @@ const sliderImages = [
   "./images/fitness5.svg"
 ];
 
-function displaySliderImage() {
-  const slider = document.querySelector('.slider');
+let slideIndex = 0;
+const slider = document.querySelector('.slider');
+const dots = document.querySelectorAll('#slider-dot .dot');
+const sliderBack = document.querySelector('.slider-back');
+const sliderForward = document.querySelector('.slider-forward');
 
-  slider.style.backgroundImage = `url("${sliderImages[1]}")`;
+sliderBack.addEventListener('click', function() {
+  const previousIndex = slideIndex;
+
+  if (slideIndex === 0) {
+    slideIndex = sliderImages.length - 1;
+  } else {
+    slideIndex--;
+  }
+
+  displaySliderImage(slideIndex);
+  fillDot(previousIndex, slideIndex);
+});
+
+sliderForward.addEventListener('click', function() {
+  const previousIndex = slideIndex;
+
+  if (slideIndex === sliderImages.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+
+  displaySliderImage(slideIndex);
+  fillDot(previousIndex, slideIndex);
+});
+
+function displaySliderImage(slideIndex) {
+  slider.style.backgroundImage = `url("${sliderImages[slideIndex]}")`;
 }
+
+function fillDot(previousIndex, currentIndex) {
+  dots[previousIndex].style.backgroundColor = '#000';
+  dots[currentIndex].style.backgroundColor = '#fff';
+}
+
+function displayInitialImageDot() {
+  slider.style.backgroundImage = `url("${sliderImages[slideIndex]}")`;
+  dots[slideIndex].style.backgroundColor = '#fff';
+}
+
+displayInitialImageDot();
