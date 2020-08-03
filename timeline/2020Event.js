3eda682df9ad74f0d9a8d88e83c3266ee3be37e5
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-03 15:16:10 -0700

    Update timeline events

--- timeline/2020Event.js
+++ timeline/2020Event.js
@@ -1,35 +1,46 @@
 export const usaData = {
   january: [
-      "COVID-19 first known case",
-    ]
-  ,
+    "COVID-19 first known case",
+  ],
   february: [
-      "Stock market plunge",
-      "Senate acquitted Trump on articles of impeachment"
-    ]
-  ,
-  march: ["Governer order stay at home and social distancing to flatten COVID-19"]
-  ,
-  april: ["Pentagon release UFO videos declassified by Navy"]
-  ,
+    "Stock market plunge",
+    "Senate acquitted Trump on articles of impeachment"
+  ],
+  march: [
+    "Governer order stay at home and social distancing to flatten COVID-19"
+  ],
+  april: [
+    "Pentagon release UFO videos declassified by Navy"
+  ],
   may: [
     "Protest against police",
-      "National Guard deployed on the streets as protests spread nationwide",
-      "SpaceX launching NASA astronauts into orbit",
-      "COVID-19 close to 2,00,000 cases and death passes 100,000"
-    ]
-  ,
-  june: ["??"]
-  ,
-  july: ["??"]
-  ,
-  august: ["??"]
-  ,
-  september: ["??"]
-  ,
-  october: ["??"]
-  ,
-  november: ["??"]
-  ,
-  december: ["??"]
+    "National Guard deployed on the streets as protests spread nationwide",
+    "SpaceX launching NASA astronauts into orbit",
+    "COVID-19 close to 2,00,000 cases and death passes 100,000"
+  ],
+  june: [
+    "Wildfire",
+    "States reopen, Coronavirus cases rise"
+  ],
+  july: [
+    "More wildfires",
+    "Mask required to enter indoor public space",
+    "Moderna begin final phase testing for COVID-19 vaccine",
+    "The Trump administration ordered hospitals to send COVID-19 data directly to the Department of Health and Human Services, bypassing the Centers for Disease Control and Prevention"
+  ],
+  august: [
+    "??"
+  ],
+  september: [
+    "??"
+  ],
+  october: [
+    "??"
+  ],
+  november: [
+    "??"
+  ],
+  december: [
+    "??"
+  ]
 }
