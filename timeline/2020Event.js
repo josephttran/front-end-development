b39baa8a570da0b6406342b09ba9a07ee7ba5532
josephttran <42635012+josephttran@users.noreply.github.com>
2020-12-20 00:44:04 -0800

    Update timeline

--- timeline/2020Event.js
+++ timeline/2020Event.js
@@ -7,7 +7,7 @@ export const usaData = {
     "Senate acquitted Trump on articles of impeachment"
   ],
   march: [
-    "Governer order stay at home and social distancing to flatten COVID-19"
+    "Governor order stay at home and social distancing to flatten COVID-19"
   ],
   april: [
     "Pentagon release UFO videos declassified by Navy"
@@ -29,18 +29,27 @@ export const usaData = {
     "The Trump administration ordered hospitals to send COVID-19 data directly to the Department of Health and Human Services, bypassing the Centers for Disease Control and Prevention"
   ],
   august: [
-    "??"
+	"Death of judge leave seat empty on supreme court",
+	"Apple first U.S. company to be value over 200 trillon",
+    "Amazon CEO Jeff Bezos first to hit net worth of 200 billon"
   ],
   september: [
-    "??"
+    "Stock market pull back",
+	"Wildfire in Oregon"
   ],
   october: [
-    "??"
+    "Resurge in COVID-19 cases",
+	"White House COVID-19 outbreak"
   ],
   november: [
-    "??"
+	"Presidential Election",
+    "Stock market increasing",
+    "UK enforced second lockdown",
+	"Tesla CEO Elon Musk overtake Bill Gate as second richest person in the World",
   ],
   december: [
-    "??"
+    "UK first to use COVID-19 vaccine",
+	"FDA approve COVID-19 vaccine",
+	"Major cyber attack"
   ]
 }
